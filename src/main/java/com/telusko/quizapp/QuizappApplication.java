9d94f09183f0cd0487a7ae845d386d8d89979198
Saim Saqib <saimmoin64@gmail.com>
2023-12-09 15:40:01 +0500

    Comment Added

--- src/main/java/com/telusko/quizapp/QuizappApplication.java
+++ src/main/java/com/telusko/quizapp/QuizappApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// Quiz Application
 public class QuizappApplication {
 
 	public static void main(String[] args) {
